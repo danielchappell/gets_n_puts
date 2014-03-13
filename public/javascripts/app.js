f6f8f232817af44dcbe85d166b015941181a5238
Joseph Chappell <jdchappe@gmail.com>
2014-03-13 02:51:26 -0500

    edit client socket listen location

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -15,7 +15,7 @@ var ChatApp = Backbone.View.extend({
     }
   },
 
-  server: io.connect(window.location.hostname)
+  server: io.connect('http://' + window.location.hostname)
 
   // form_submit: function(e){
   //   e.preventDefault();
